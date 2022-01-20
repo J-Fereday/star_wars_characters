b246724a1f6030b3f5a0eb305b0b4382910b48f4
J-Fereday <97160923+J-Fereday@users.noreply.github.com>
2022-01-20 10:24:52 +0000

    Update SW_pymongo.py

--- SW_pymongo.py
+++ SW_pymongo.py
@@ -3,85 +3,51 @@ import requests
 import json
 
 
-
-
 def connection(server):
+    #connect to MongoDB server
     client = pymongo.MongoClient()
     return client[server]
 
 
-def insertingShips(URL):
-    #get mongo server connection
+def getDatafromURL(URL):
+    #getData from URL and return
+    request = requests.get(URL).text
+    return json.loads(request)
+
+
+def InsertingData(URL,server):
+    #open database and retrieve data from URL using existing functions
+    db = connection(server)
+    data = getDatafromURL(URL)
+    for item in data["results"]:
+        db.starships.insert_one(item)
+
+
+def updatingShips():
+    #connect to database
     db = connection(server="starwars")
     
-    #get text from URL and convert to json format
-    r = requests.get(URL).text
-    d = json.loads(r)
-    
-    #loop over ship types in data set
-    for ships in d["results"]:
-        db.starships.insert_one(ships)
-        
-        #update pilot names
-        name_array = []
-        for pilots in ships["pilots"]:
-            name_array.append(replaceURLObjectID(pilots))
-        db.starships.update_one({"name":ships["name"]},{"$set":{"pilots":name_array}})
-        
-    #if next is not empty, load data from that URL
-    if d["next"] is not None:
-        insertingShips(URL=d["next"])
-
-    
-
-def replace_name_with_ID():
-    #get mongo server connection
-    db = connection(server="starwars")
-    
-    #get starships and loop through all ships
-    starships = db.starships.find()
-    for ships in starships:
+    #loop through ships
+    for ships in db.starships.find():
         
         #make empty id array
         id_array = []
         
-        #loop through all pilots
+        #loop through pilots
         for pilots in ships["pilots"]:
-            #find id for given pilot name
-            result = db.characters.find_one({"name":pilots})
-            id_array.append(result["_id"])
-            
-        #replace names with ids
+           #get pilot name from URL in data, use name to search id in database and append to the array
+           pilot_name = getDatafromURL(pilots)["name"]
+           result = db.characters.find_one({"name":pilot_name})
+           id_array.append(result["_id"])
+           
+        #update database with array
         db.starships.update_one({"name":ships["name"]},{"$set":{"pilots":id_array}})
-
-
-
-def replaceURLObjectID(URL):
-    r = requests.get(URL).text
-    d = json.loads(r)
-    return d["name"]
 
 
 db = connection(server="starwars")
 
 db.starships.drop()
 
+InsertingData(URL="https://swapi.dev/api/starships",server="starwars")
 
-insertingShips("https://swapi.dev/api/starships")
-
-replace_name_with_ID()
-
-
-
-
-
-
-
-
-
-
-
-
-#combining functions
-#db.starships.aggregate([{$lookup:{from:"characters",localField:"commander",foreignField:"_id",as:"matched_pilot}}]}
-#
+updatingShips()
